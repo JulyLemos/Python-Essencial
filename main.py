41d3e51707ac476798a075bd1bd23fc6d7a6a94b
July <julylemos1@hotmail.com>
2025-01-08 22:50:55 -0300

    Exercícios 12 a 15

--- main.py
+++ main.py
@@ -45,7 +45,6 @@ def exe1_6():
     num = int(input("Digite um número: "))
     print(f"A raiz quadrada do número é {num*num}")
 
-#07/01/2025
 def exe1_7():
     #Crie um programa que peça ao usuário para digitar um número inteiro. Em seguida, o programa deve calcular e 
     # mostrar o valor dos inteiros anterior e posterior a esse número.
@@ -95,7 +94,43 @@ def exe1_11():
     raio = int(input("Digite o raio do círculo: "))
     print(f"A área do círculo é {math.pi*(raio**2):.2f}") #por estar usando a biblioteca math.pi, o valor vai tender para acima
     print(f"O comprimento do círculo é {2 * math.pi * raio:.2f}") #se fosse 3.14 definido, o valor seria um pouco mais baixo
-    
+
+#08/01/2025
+def exe1_12():
+    #Crie um programa que peça ao usuário para digitar as dimensões de um retângulo (largura e altura). 
+    #Em seguida, o programa deve calcular e mostrar a área e o perímetro desse retângulo.
+    larg = int(input("Digite a largura do retângulo: "))
+    altu = int(input("Digite a altura do retângulo: "))
+    area = larg * altu
+    perim = larg*2 + altu * 2
+    print(f"A área do retâgulo é {area} e o perímetro do retângulo é {perim}")
+
+def exe1_13():
+    #Crie um programa que peça ao usuário para digitar a base e a altura de um triângulo. Em seguida, o programa deve calcular e mostrar a área desse triângulo.
+    base = int(input("Digite a base do triângulo: "))
+    altu = int(input("Digite a altura do triângulo: "))
+    area = (base*altu)/2
+    print(f"A área do triãngulo é {area}")
+
+def exe1_14():
+    #Crie um programa que peça ao usuário para digitar o nome, o preço de custo, o preço de venda e a quantidade em estoque de determinado produto. 
+    #Em seguida, o programa deve calcular e mostrar o lucro que esse produto pode gerar se todo o estoque for vendido.
+    nome = input("Digite o nome do produto: ")
+    p_custo = float(input("Digite o preço de custo do produto: "))
+    p_venda = float(input("Digite o preço de venda do produto: "))
+    estoq = int(input("Digite a quantidade em estoque od produto: "))
+    lucro = (p_venda*estoq) - (p_custo*estoq)
+    print(f"Caso {nome} seja tenha todo o estoque vendido, o lucro será de R${lucro}")
+
+def exe1_15():
+    #Crie um programa que peça ao usuário para digitar o valor total de uma venda, o percentual de desconto aplicado e o percentual de imposto cobrado. 
+    #Ao fim, o programa deve mostrar o preço final da mercadoria, sendo que o imposto é cobrado sobre o valor com desconto.
+    val_tot = float(input("Digite o valor total da venda: "))
+    desc = float(input("Digite o percentual de desconto do produto: "))
+    imposto = float(input("Digite o percentual do imposto: "))
+    v_final = val_tot*(1-desc/100)*(1+imposto/100)
+    print(f"O produto teve valor final de R${v_final:.2f}")
+
 
 
 
@@ -104,7 +139,7 @@ def menu():
     while True:
         time.sleep(2)
         print("\n")
-        for i in range(1,12):
+        for i in range(1,16):
             print(f"Questão {i}")
 
         op = int(input("\nDigite uma opção:"))
@@ -132,7 +167,42 @@ def menu():
                 exe1_10()
             case 11:
                 exe1_11()
+            case 12:
+                exe1_12()
+            case 13:
+                exe1_13()
+            case 14:
+                exe1_14()
+            case 15:
+                exe1_15()
 
 menu()
 
 #Por enquanto está assim, por conta de não está conseguindo importar outro arquivo para o main
+
+# Exercício 1.16 (Difícil):
+# Crie um programa que peça ao usuário para digitar a massa e a aceleração de um objeto. Em seguida, o programa deve calcular e mostrar a força resultante.
+
+# Exercício 1.17:
+# Crie um programa que peça ao usuário para digitar a velocidade inicial, a velocidade final e o tempo de transição de uma velocidade para a outra. Em seguida, o programa deve calcular e mostrar a aceleração.
+
+# Exercício 1.18:
+# Crie um programa que peça ao usuário para digitar o valor da medida de um ângulo em radianos. Em seguida, o programa deve calcular e mostrar o valor desse ângulo em graus.
+
+# Exercício 1.19:
+# Crie um programa que peça ao usuário para digitar o comprimento de dois lados de um triângulo retângulo. Em seguida, o programa deve calcular e mostrar o comprimento da hipotenusa.
+
+# Exercício 1.20:
+# Crie um programa que peça ao usuário para digitar a distância percorrida por um objeto e o tempo gasto no trajeto. Em seguida, o programa deve calcular e mostrar a velocidade média do objeto.
+
+# Exercício 1.21 (Difícil):
+# Crie um programa que peça ao usuário para digitar a distância percorrida, o tempo gasto e a aceleração de um objeto. Em seguida, o programa deve calcular e mostrar a velocidade inicial e final.
+
+# Exercício 1.22:
+# Crie um programa que calcule e mostre o perímetro de um círculo dado o seu raio como entrada.
+
+# Exercício 1.23:
+# Crie um programa que calcule e mostre o volume de uma esfera dado o seu raio como entrada.
+
+# Exercício 1.24:
+# Crie um programa que calcule e mostre a área de um triângulo retângulo dadas as medidas dos seus catetos como entrada.
